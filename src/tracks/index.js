c50d0c973da9340d6245e1e39991c6428f199658
Rob Abarrota <rob.abarrota@gmail.com>
2022-08-01 21:22:03 -0400

    scroll tracks

--- src/tracks/index.js
+++ src/tracks/index.js
@@ -6,23 +6,25 @@ import { isEmpty } from 'lodash';
 
 const Tracks = () => {
 	const dispatch = useDispatch();
-	const {content: trackList, loading: trackListLoading} = useSelector(getTrackList);
+	const { content: trackList, loading: trackListLoading } = useSelector(getTrackList);
 
-	if(isEmpty(trackList) && !trackListLoading) dispatch(fetchTrackList());
+	if (isEmpty(trackList) && !trackListLoading) dispatch(fetchTrackList());
 
 	if (trackList) {
 		return (
 			<div>
 				<h1>Tracks</h1>
-				<table>
-					<tbody>
-						{trackList.map((track) => (
-							<tr>
-								<td key={track}>{track}</td>
-							</tr>
-						))}
-					</tbody>
-				</table>
+				<div className="table-container">
+					<table>
+						<tbody>
+							{trackList.map((track) => (
+								<tr>
+									<td key={track}>{track}</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
+				</div>
 			</div>
 		);
 	}
