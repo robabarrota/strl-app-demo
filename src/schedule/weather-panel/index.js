db9d985fe188ea8a57c52f1b8ee8989d2a7798b9
robabarrota <44911734+robabarrota@users.noreply.github.com>
2023-09-09 12:00:37 -0400

    Implement default weather when next is not populated (#77)

--- src/schedule/weather-panel/index.js
+++ src/schedule/weather-panel/index.js
@@ -1,5 +1,5 @@
 import './styles.scss';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { weatherIconMap, weatherColorMap } from 'src/utils/constants';
 import useIsMobile from 'src/hooks/useIsMobile';
@@ -40,10 +40,35 @@ const WeatherIcon = styled.img`
     height: 4rem;
     width: 4rem;
     margin: auto;
+
+    transition: opacity 2s ease-in;
+    -webkit-transition: opacity 2s ease-in;
+    -moz-transition: opacity 2s ease-in;
+    -ms-transition: opacity 2s ease-in;
+    -o-transition: opacity 2s ease-in;
+    opacity: 1;
+    ${
+        props => props.transitioning ? 
+        `transition: opacity 2s ease-out;
+        -webkit-transition: opacity 2s ease-out;
+        -moz-transition: opacity 2s ease-out;
+        -ms-transition: opacity 2s ease-out;
+        -o-transition: opacity 2s ease-out;
+        opacity: 0;` :
+        '' 
+    }
 `;
+
+const tbdWeatherIconTime = 2000;
+
+const weatherIcons = Object.values(weatherIconMap);
 
 const WeatherPanel = ({trackInfo}) => {
     const isMobile = useIsMobile();
+
+	const [tbdWeatherIcon, setTbdWeatherIcon] = useState({iconIndex: 0, src: weatherIcons[0]});
+	const [isTbdIconTransitioning, setIsTbdIconTransitioning] = useState(false);
+
     const qualifyingWeatherGradientStages = [
         weatherColorMap[trackInfo?.q1Weather],
         weatherColorMap[trackInfo?.q2Weather],
@@ -53,18 +78,34 @@ const WeatherPanel = ({trackInfo}) => {
         weatherColorMap[trackInfo?.r2Weather],
         weatherColorMap[trackInfo?.r3Weather],
     ];
+
+    useEffect(() => {
+        const timer1 = setTimeout(() => {
+            if (isTbdIconTransitioning) {
+                const nextIconIndex = (tbdWeatherIcon?.iconIndex + 1) % weatherIcons.length;
+                const nextIconSrc = weatherIcons[nextIconIndex];
+                setTbdWeatherIcon({iconIndex: nextIconIndex, src: nextIconSrc});
+                setIsTbdIconTransitioning(false);
+            } else {
+                setIsTbdIconTransitioning(true);
+            }
+        }, tbdWeatherIconTime);
+        return () => clearTimeout(timer1);
+        
+    }, [isTbdIconTransitioning, setIsTbdIconTransitioning])
+
 	return (
         <div className="weather-panel">
             <Stint gradientStages={qualifyingWeatherGradientStages}>
                 <Title>Qualifying</Title>
                 <WeatherRow>
                     <WeatherItem isMobile={isMobile} isFirst>
-                        <WeatherLabel>{trackInfo?.q1Weather}</WeatherLabel>
-                        <WeatherIcon src={weatherIconMap[trackInfo?.q1Weather]}/>
+                        <WeatherLabel>{trackInfo?.q1Weather || 'TBD'}</WeatherLabel>
+                        <WeatherIcon transitioning={!weatherIconMap[trackInfo?.q1Weather] && isTbdIconTransitioning} src={weatherIconMap[trackInfo?.q1Weather] || tbdWeatherIcon.src}/>
                     </WeatherItem>
                     <WeatherItem isMobile={isMobile}>
-                        <WeatherLabel>{trackInfo?.q2Weather}</WeatherLabel>
-                        <WeatherIcon src={weatherIconMap[trackInfo?.q2Weather]}/>
+                        <WeatherLabel>{trackInfo?.q2Weather || 'TBD'}</WeatherLabel>
+                        <WeatherIcon transitioning={!weatherIconMap[trackInfo?.q2Weather] && isTbdIconTransitioning} src={weatherIconMap[trackInfo?.q2Weather] || tbdWeatherIcon.src}/>
                     </WeatherItem>
                 </WeatherRow>
             </Stint>
@@ -72,16 +113,16 @@ const WeatherPanel = ({trackInfo}) => {
                 <Title>Race</Title>
                 <WeatherRow>
                     <WeatherItem isMobile={isMobile} isFirst>
-                        <WeatherLabel>{trackInfo?.r1Weather}</WeatherLabel>
-                        <WeatherIcon src={weatherIconMap[trackInfo?.r1Weather]}/>
+                        <WeatherLabel>{trackInfo?.r1Weather || 'TBD'}</WeatherLabel>
+                        <WeatherIcon transitioning={!weatherIconMap[trackInfo?.r1Weather] && isTbdIconTransitioning} src={weatherIconMap[trackInfo?.r1Weather] || tbdWeatherIcon.src}/>
                     </WeatherItem>
                     <WeatherItem isMobile={isMobile}>
-                        <WeatherLabel>{trackInfo?.r2Weather}</WeatherLabel>
-                        <WeatherIcon src={weatherIconMap[trackInfo?.r2Weather]}/>
+                        <WeatherLabel>{trackInfo?.r2Weather || 'TBD'}</WeatherLabel>
+                        <WeatherIcon transitioning={!weatherIconMap[trackInfo?.r2Weather] && isTbdIconTransitioning} src={weatherIconMap[trackInfo?.r2Weather] || tbdWeatherIcon.src}/>
                     </WeatherItem>
                     <WeatherItem isMobile={isMobile}>
-                        <WeatherLabel>{trackInfo?.r3Weather}</WeatherLabel>
-                        <WeatherIcon src={weatherIconMap[trackInfo?.r3Weather]}/>
+                        <WeatherLabel>{trackInfo?.r3Weather || 'TBD'}</WeatherLabel>
+                        <WeatherIcon transitioning={!weatherIconMap[trackInfo?.r3Weather] && isTbdIconTransitioning} src={weatherIconMap[trackInfo?.r3Weather] || tbdWeatherIcon.src}/>
                     </WeatherItem>
                 </WeatherRow>
             </Stint>
